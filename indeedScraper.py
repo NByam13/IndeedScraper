6e332b6c732fe58c7a93e9fe4c64f2d37ac5b63e
Tangent1al <y.dydxx.b@gmail.com>
2021-01-18 10:58:08 -0500

    small updates relating to start up args. Making it so it is interactive.

--- indeedScraper.py
+++ indeedScraper.py
@@ -1,8 +1,25 @@
+import sys
 import requests
 from bs4 import BeautifulSoup
 
+url = ''
+
+#First we need to gather command line arguments, if there are none we're going to end the script
+if len(sys.argv) == 1:
+	print('Please specify which site to search and what keywords to search for.') #don't really like this, maybe ask for their preferred keywords
+	exit(0)
+	
+elif len(sys.argv) == 2 and sys.argv[1].startswith('-url'): # if a site is used as an arg, decide if it is indeed or monster
+	if sys.argv[1].find('indeed') != -1 :
+		url = 'https://ca.indeed.com/jobs?'
+	elif sys.argv[1].find('monster') != -1:
+		url = 'https://www.monster.ca/jobs/search/?'
+	else:
+		print('That job search website is currently unsupported. Please use either Indeed or Monster')
+		# get either indeed or monster
+	
 # get the data from the url in the form of html sent after a request to their server
-url = 'https://ca.indeed.com/jobs?q=software developer&l=Waterloo, ON&radius=25'
+url = 'https://ca.indeed.com/jobs?q=software developer&l=Waterloo,ON&radius=25'
 page = requests.get(url)
 
 # Create a soup object of the html that we can parse through to find what I want
